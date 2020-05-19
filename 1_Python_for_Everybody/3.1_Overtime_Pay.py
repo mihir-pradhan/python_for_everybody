f2a2417bf50609f3baf87e48551b4311dc64103e
Mihir Pradhan <mihirpradhan2004@gmail.com>
2020-05-19 17:03:11 -0500

    Updated 3.1_Overtime_Pay.py

--- 1_Python_for_Everybody/3.1_Overtime_Pay.py
+++ 1_Python_for_Everybody/3.1_Overtime_Pay.py
@@ -1,18 +1,19 @@
 # Calculate Pay - Regular pay for up to 40 hours and 1.5 times pay for all hours above 40 hours
 
 hours = input("Enter Hours: ")
-rate = input("Enter pay rate: ")
+rate = input("Enter Rate: ")
 
-hours = float(hours)
-rate = float(rate)
+try:
+    hours = float(hours)
+    rate = float(rate)
+except:
+    print("Error, please enter numeric input")
 
-if (hours <= 40):
-    pay = hours * rate
-else:
+if (hours > 40):
     regular_pay = rate * 40
-    overtime_hours = hours - 40
-    overtime_rate = rate * 1.5
-    overtime_pay = overtime_hours * overtime_rate
+    overtime_pay = (hours - 40) * (rate * 1.5)
     pay = regular_pay + overtime_pay
+else:
+    pay = hours * rate
 
-print(pay)
+print("Pay:", pay)
