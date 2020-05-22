72eef1890570303a2ad18071b47a926d4b88ab97
Mihir Pradhan <mihirpradhan2004@gmail.com>
2020-05-21 21:51:00 -0500

    Added assignment 5.2

---
+++ 1_Python_for_Everybody/5.2_Compute_Max_and_Min.py
@@ -0,0 +1,28 @@
+# Prompt user to enter integer numbers and print out Maximum and Minimum
+
+largest = None
+smallest = None
+
+while True:
+    num = input('Enter a number: ')
+    if num == 'done': 
+        break
+
+    try:
+        num = int(num)
+    except:
+        print('Invalid input')
+        continue
+
+    if largest is None:
+        largest = num
+    elif num > largest:
+        largest = num
+    
+    if smallest is None:
+        smallest = num
+    elif num < smallest:
+        smallest = num
+
+print("Maximum is", largest)
+print("Minimum is", smallest)
