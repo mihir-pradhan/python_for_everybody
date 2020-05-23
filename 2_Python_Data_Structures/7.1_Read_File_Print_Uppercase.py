14cd8e94e892804223b592513d1cd631036ef91a
Mihir Pradhan <mihirpradhan2004@gmail.com>
2020-05-23 12:14:11 -0500

    Updated assignment 7.1 to use for loop

--- 2_Python_Data_Structures/7.1_Read_File_Print_Uppercase.py
+++ 2_Python_Data_Structures/7.1_Read_File_Print_Uppercase.py
@@ -2,7 +2,7 @@
 
 filename = input('Enter name of the file to open: ')
 file = open(filename)
-readfile = file.read()
-readfile = readfile.rstrip()
-file_up = readfile.upper()
-print(file_up)
+
+for line in file:
+    line = line.rstrip()
+    print(line.upper())
